0efd8b11687180a9424ee3926db909e11f6676b7
konojunya <junya03dance@gmail.com>
2017-09-26 00:49:58 +0900

    モックサーバを書く

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("server")
+import * as express from 'express';
+import * as cookieParser from 'cookie-parser';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.json({
+    hoge: 'hoge'
+  });
+});
+
+app.listen(PORT);
